ad3307c28e3c38930c67a791eafaaf928e0e9e0f
mesemi <mesemigame@gmail.com>
2025-09-24 02:16:26 -0400

    Fix thing i forgot, but its all good now

--- src/main/java/me/legotijger/bfv/listener/FoodConsumeListener.java
+++ src/main/java/me/legotijger/bfv/listener/FoodConsumeListener.java
@@ -10,7 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
